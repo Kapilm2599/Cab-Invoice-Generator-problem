9983d198a4b1b68ddb62a1ff73d2f0b0670f4fec
Kapilm2599 <mahajankapil575@gmail.com>
2023-06-01 20:15:22 +0530

    Added: Generate invoice

--- src/main/java/org/bridgelabz/CabInvoice.java
+++ src/main/java/org/bridgelabz/CabInvoice.java
@@ -8,17 +8,19 @@ public class CabInvoice {
 
 	public double calculateFare(double distance, int time) {
 		double totalFare = distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
-		if (totalFare < MINIMUM_FARE)
-			return MINIMUM_FARE;
-		else
-			return totalFare;
+		return Math.max(totalFare, MINIMUM_FARE);
 	}
-	
+
 	public double calculateFare(Ride[] rides) {
 		double totalFare = 0;
 		for (Ride ride : rides) {
 			totalFare += this.calculateFare(ride.distance, ride.time);
 		}
 		return totalFare;
+	}
+
+	public InvoiceSummary getInvoiceSummary(Ride[] rides) {
+		double totalFare = calculateFare(rides);
+		return new InvoiceSummary(rides.length, totalFare);
 	}
 }
